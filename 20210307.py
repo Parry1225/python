169b824bd6c4463bbe0c59293acd72ae108157af
Parry1225 <htst10510@htes.tp.edu.tw>
2021-03-07 19:17:29 +0800

    20210307 功課

--- 20210307.py
+++ 20210307.py
@@ -5,7 +5,12 @@
 #print('c:',pi)
 
 L=input('今天天氣如何')#輸入文字
+print('天氣是陰天:不是',L)
 W=input('今天氣溫如何')
-K=input('今天氣溫如何')
-print('天氣是:不是',L)
-print('氣溫是:不是',W)
+print('氣溫是9999:不是',W)
+K=input('今天運氣好不好')
+print('遇到我這台機器運氣就好')
+O=input('你IQ多少')
+print('怎麼可能那麼高不可能是',O,'是吧')
+P=input('今天開不開心')
+O=input('遇到我就應該開心啊')
