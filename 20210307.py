2f03c5aa9879e7de391b173dbf01598b1c6d8194
Parry1225 <htst10510@htes.tp.edu.tw>
2021-03-07 11:27:13 +0800

    09090

---
+++ 20210307.py
@@ -0,0 +1,11 @@
+#print('a:',1+2+3+4+5)
+#print('b:',1*2-3/4)
+#print('a:','What?')
+#pi=3.14
+#print('c:',pi)
+
+L=input('今天天氣如何')#輸入文字
+W=input('今天氣溫如何')
+K=input('今天氣溫如何')
+print('天氣是:不是',L)
+print('氣溫是:不是',W)
