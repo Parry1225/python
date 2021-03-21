f6e5a3b96f2c34cb3f8282c3ae2b8f88db36dd2e
Parry1225 <htst10510@htes.tp.edu.tw>
2021-03-21 20:18:02 +0800

    6664

--- homework/20210321/t0_fraction.py
+++ homework/20210321/t0_fraction.py
@@ -1,14 +1,17 @@
-"""
-Topic:輸入分子及分母，確認是否等於 350/450:
+#"""
+#Topic:輸入分子及分母，確認是否等於 350/450:
 
-Show:Please input numerator"
-Input1:70
+#Show:Please input numerator"
+#Input1:70
 
-show:Please input Denominator:
-Input2:90
-Output:True
+#show:Please input Denominator:
+#Input2:90
+#Output:True
 
-Input1:6
-Input2:9
-Output:False
-"""
+#Input1:6
+#Input2:9
+#Output:False
+#"""
+A=int(input('請輸入分子'))
+B=int(input('請輸入分母'))
+print((A/B) == (350/450))
--- homework/20210321/t2_climate.py
+++ homework/20210321/t2_climate.py
@@ -1,8 +1,16 @@
 """
-Topic:輸入溫度，如果溫度>=40度C,顯示: 太熱, 
+Topic:輸入溫度，如果溫度>=40度C,顯示: 太熱,
 　　　如果溫度<= 10 顯示:太冷, 其他：舒適:
 
 Show:Please input temperature:"
 Input1:40
 Output:It's too hot.
 """
+a=int(input('輸入溫度'))
+if a<=10:
+    print('太冷')
+elif a>=40:
+    print('太熱')
+elif a<40 and a>10:
+    print('舒適')
+
